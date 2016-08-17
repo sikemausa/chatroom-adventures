e371a028432bbe8e79a984768cf5d9396e89008c
Casey Metz <caseymetz@gmail.com>
2016-08-16 18:18:21 -0600

    first scripts and html

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const index = require('../../lib/index.js');
 
 describe('welcome page', function(){
   it('webpage should have a title', function(){
@@ -8,5 +9,21 @@ describe('welcome page', function(){
     var title = browser.getTitle();
     console.log(title);
     assert.equal(title, 'Holla!');
+  });
+  it('homepage should have an h1', function(){
+    browser.url('/');
+    var content = browser.getText('h1');
+    assert.equal(content, 'Holla!');
+  });
+  it('homepage send button should be disabled by default', function(){
+    var value = browser.isEnabled('#send');
+    assert.equal(value, false);
+  });
+  it('homepage send button should be enabled when the input contains text', function(){
+    var input = browser.element('#input');
+    input.setValue('words');
+    var value = browser.isEnabled('#send');
+    enabledButton();
+    assert.equal(value, true);
   });
 });
--- test/test-index.js
+++ test/test-index.js
@@ -1,7 +1,19 @@
 const assert = require('chai').assert
+const index = require('../lib/index.js');
 
 describe('our test bundle', function () {
   it('should work', function () {
-    assert(true)
+    assert(true);
+  });
+  it('should see our index.js file', function(){
+    assert.equal(newThing, true);
   });
 });
+//
+// describe('our button', function(){
+//   it('should enable when text is entered in input', function(){
+//     var someField = true;
+//     buttonEnabler(someField, sendButton);
+//     assert.equal(sendButton.attr('disabled', false), true);
+//   });
+// });
